32babe27d6d6037e423d1eb3b354292605180b72
Doxygen CI <travis@travis-ci.org>
2020-11-15 04:21:46 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['getbattprofile',['getBattProfile',['../class_adafruit___l_c709203_f.html#a3f50219a24fc631f03e4c190bae6b339',1,'Adafruit_LC709203F']]],
   ['getcelltemperature',['getCellTemperature',['../class_adafruit___l_c709203_f.html#a35c6a876a8917c4cf028c871509982fe',1,'Adafruit_LC709203F']]],
   ['geticversion',['getICversion',['../class_adafruit___l_c709203_f.html#a68a6117229db11b4a05d2956df8f334d',1,'Adafruit_LC709203F']]],
   ['getthermistorb',['getThermistorB',['../class_adafruit___l_c709203_f.html#a03bc3dd5735030a1ed447d978d2dbdc1',1,'Adafruit_LC709203F']]]
