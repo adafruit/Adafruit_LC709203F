6e5a77114871349444019d7179533d55caee1e2e
Doxygen CI <ci-arduino@invalid>
2022-02-12 13:12:12 +0000

    Deploy docs to GitHub Pages from commit d1ce6ce39a
    Commit: d1ce6ce39a8d300ae9aa3545f7ad6f2e0081986d
    GitHub Actions run: 1833825197

--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -3,6 +3,7 @@ var searchData=
   ['setalarmrsoc',['setAlarmRSOC',['../class_adafruit___l_c709203_f.html#a7eb61a678bb8076e02c22822209d1cb7',1,'Adafruit_LC709203F']]],
   ['setalarmvoltage',['setAlarmVoltage',['../class_adafruit___l_c709203_f.html#ae9640005ed0e81d9c1f9741eded1d9aa',1,'Adafruit_LC709203F']]],
   ['setbattprofile',['setBattProfile',['../class_adafruit___l_c709203_f.html#a71648957404abc720cc63e8206b7aa57',1,'Adafruit_LC709203F']]],
+  ['setpackapa',['setPackAPA',['../class_adafruit___l_c709203_f.html#a85350d88e08fcc0742d0d3612a6a7e4a',1,'Adafruit_LC709203F']]],
   ['setpacksize',['setPackSize',['../class_adafruit___l_c709203_f.html#ac6aec446bb026a1bdd5abde97f66d254',1,'Adafruit_LC709203F']]],
   ['setpowermode',['setPowerMode',['../class_adafruit___l_c709203_f.html#a8daa8545cbcc40f28fd22134dcc9a067',1,'Adafruit_LC709203F']]],
   ['settemperaturemode',['setTemperatureMode',['../class_adafruit___l_c709203_f.html#a9aed9f7c8059a0991ca80b67dc8f5d01',1,'Adafruit_LC709203F']]],
